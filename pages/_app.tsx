8d5d5acef618de7ad16c20b404272281fce43eef
dzulfikri.maulana <dzuuuuul2@gmail.com>
2024-11-03 23:09:33 +0700

    remove firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,75 +1,74 @@
-import 'antd/dist/antd.css'
-import '../styles/globals.css'
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
+import "antd/dist/antd.css";
+import "../styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 // import { SessionProvider } from "next-auth/react"
 import { AppProvider } from "../context/AppContext";
 import { SWRConfig } from "swr";
 import Head from "next/head";
 import axios from "axios";
-import { firebaseCloudMessaging } from '@lib/firebase/webPush';
-import { getMessaging, onMessage } from "firebase/messaging";
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }: AppPropsWithLayout) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
   // return <Component {...pageProps} />
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  useEffect(() => {
-    setToken();
-    async function setToken() {
-      try {
-        const token = await firebaseCloudMessaging.init();
-        if (token) {
-          getMessage();
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    function getMessage() {
-      const messaging = getMessaging();
-      // console.log({ messaging });
-      onMessage(messaging, (payload) => {
-        // console.log('Message received. ', payload);
-        // ...
-        const data: any = payload.data 
-        const { title, body } = JSON.parse(data?.notification);
-        var options = {
-          body,
-        };
-        self.registration.showNotification(title, options);
-      });
-    }
-  });
+  // useEffect(() => {
+  //   setToken();
+  //   async function setToken() {
+  //     try {
+  //       const token = await firebaseCloudMessaging.init();
+  //       if (token) {
+  //         getMessage();
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   function getMessage() {
+  //     const messaging = getMessaging();
+  //     // console.log({ messaging });
+  //     onMessage(messaging, (payload) => {
+  //       // console.log('Message received. ', payload);
+  //       // ...
+  //       const data: any = payload.data;
+  //       const { title, body } = JSON.parse(data?.notification);
+  //       var options = {
+  //         body,
+  //       };
+  //       self.registration.showNotification(title, options);
+  //     });
+  //   }
+  // });
 
   return (
     // `session` comes from `getServerSideProps` or `getInitialProps`.
     // Avoids flickering/session loading on first load.
     // <SessionProvider session={session} refetchInterval={5 * 60}>
-      <SWRConfig value={{ fetcher: (url) => axios(url).then((r) => r.data) }}>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>ESS - Dashboard</title>
-        </Head>
-        <AppProvider>
-          {getLayout(<Component {...pageProps} />)}
-        </AppProvider>
-      </SWRConfig>
+    <SWRConfig value={{ fetcher: (url) => axios(url).then((r) => r.data) }}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>ESS - Dashboard</title>
+      </Head>
+      <AppProvider>{getLayout(<Component {...pageProps} />)}</AppProvider>
+    </SWRConfig>
     // </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
